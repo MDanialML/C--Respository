f29ad8cde0ae4a77b633cca9ae4bdd753eac5e5c
M Danial <m.danial465@gmail.com>
2024-03-04 19:30:43 +0500

    check for Palindrome second approach

--- linked_list14.cpp
+++ linked_list14.cpp
@@ -87,6 +87,62 @@ void firstApproach(vector<int> &arr,Node* head)
     cout <<"Link list is valid palindrome.!"<<endl;
 }
 
+//get middle node
+Node* getMiddle(Node* head)
+{
+    Node* slow = head;
+    Node* fast = head -> next;
+    while(fast != NULL && fast -> next != NULL)
+    {
+        fast = fast -> next -> next;
+        slow = slow -> next;
+    }
+    return slow;
+}
+
+//reverse second half of 
+Node* reversedHalf(Node* head)
+{
+    Node* previous = NULL;
+    Node* current = head;
+    Node* next = 0;
+    while(current != NULL)
+    {
+        next = current -> next;
+        current -> next = previous;
+        previous = current;
+        current = next;
+    }
+    return previous;
+}
+
+//check palindrome
+bool isPalindrome(Node* head)
+{
+    Node* mid = getMiddle(head);
+    Node* temp = mid->next;
+    mid->next = reversedHalf(temp);
+    // cout <<"check wheter second half of link list is reversed or not"<<endl;
+    //check valid palindrome
+    Node* head1 = head;
+    Node* head2 = mid -> next;
+    while(head2 != NULL)
+    {
+        if (head1->data != head2 -> data)
+        {
+            return false;
+        }
+        head1 = head1 -> next;
+        head2 = head2 -> next;
+    }
+
+    // temp = mid->next;
+    // mid->next = reversedHalf(temp);
+
+    return true;
+    // cout<<"Link list is valud Palindrome"<<endl;
+    // print (head);
+}
 //driver function
 int main()
 {
@@ -102,8 +158,19 @@ int main()
     print(head);
 
     //first approach
-    vector<int> arr;
-    firstApproach(arr,head);
+    // vector<int> arr;
+    // firstApproach(arr,head);
+    // Node* middle = getMiddle(head);
+    // cout <<"Middle node :   "<<middle->data<<endl;
+    if(isPalindrome(head))
+    {
+        cout <<"Linked list is palindrome"<<endl;
+    }
+    else
+    {
+        cout <<"Linked list is not a palindrome"<<endl;
+    }
+    print(head);
 
     return 0;
 }
